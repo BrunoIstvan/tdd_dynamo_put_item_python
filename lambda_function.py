f32f35a78fa4f5d284df2d828e3ecba257ee5932
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2021-10-04 06:59:32 -0300

    fourth commit

--- lambda_function.py
+++ lambda_function.py
@@ -6,16 +6,16 @@ from app.process import Process
 def lambda_handler(event, context):
     return Process().execute(event)
 
-
-if __name__ == '__main__':
-    event = {
-        'body': json.dumps({
-            'cpf': '12345678900',
-            'email': 'usuario1@gmail.com',
-            'full_name': 'usuario 1 da silva',
-            'state': 'SP',
-            'city': 'São Paulo'
-        })
-    }
+#
+# if __name__ == '__main__':
+#     event = {
+#         'body': json.dumps({
+#             'cpf': '12345678900',
+#             'email': 'usuario1@gmail.com',
+#             'full_name': 'usuario 1 da silva',
+#             'state': 'SP',
+#             'city': 'São Paulo'
+#         })
+#     }
 
     lambda_handler(event, None)
